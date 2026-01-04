621391f15d6ccbb57c612c553965678cc915a626
Kartikay Tewari <tewarinaman77@gmail.com>
2026-01-05 00:29:06 +0530

    Java file
    Return number in the array that have repeated exactly 1 + half the size of the array

--- LT961.java
+++ LT961.java
@@ -1,1 +1,11 @@
-class Solution
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (!seen.add(num)) {
+                return num;
+            }
+        }
+        return -1;
+    }
+}
