1f7c465739607cf3651484f266b9de2d681ebbf4
Kartikay Tewari <tewarinaman77@gmail.com>
2026-01-08 20:53:22 +0530

    Java file
    return the maximum relative dot multiplication between subsets of two array

--- LT1458.java
+++ LT1458.java
@@ -1,16 +1,18 @@
-class Solution{
-    int ans=Integer.MIN_VALUE;
-    public int maxDotProduct(int[] a,int[] b){
-    dfs(a,b,0,0,false,0);
-        return ans;
-    }
-    void dfs(int[] a, int[] b, int i, int j, boolean used, int sum){
-        if(i==a.length || j==b.length){
-        if(used) ans=Math.max(ans,sum);
-        return;
-    }
-    dfs(a, b, i+1, j, used, sum);
-    dfs(a, b, i, j+1, used, sum);
-    dfs(a, b, i+1, j+1, true, sum+a[i]*b[j]);
+class Solution {
+    public int maxDotProduct(int[] a, int[] b) {
+        int n = a.length, m = b.length;
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 0; i <= n; i++){
+            for (int j = 0; j <= m; j++){
+                dp[i][j] = Integer.MIN_VALUE;
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                int prod = a[i - 1] * b[j - 1];
+                dp[i][j] = Math.max(prod, Math.max(dp[i - 1][j - 1] == Integer.MIN_VALUE ? prod : dp[i - 1][j - 1] + prod, Math.max(dp[i - 1][j], dp[i][j - 1])));
+            }
+        }
+        return dp[n][m];
     }
 }
