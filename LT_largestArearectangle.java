1a48177911b04e346a4214aca44427664997d274
Kartikay Tewari <tewarinaman77@gmail.com>
2026-02-02 23:06:35 +0530

    Java file
    Find the largest rectangle area that can be formed by given histogram using stack

---
+++ LT_largestArearectangle.java
@@ -0,0 +1,28 @@
+public int largestArearectangle(int[] heights){
+    Stack<Integer> stack = new Stack<>();
+    int max = 0;
+
+    stack.push(0);
+
+    for (int i = 1; i < heights.length; i++) {
+        while((!stack.isEmpty()) && heights[i] < heights[stack.peek()]){
+            max = getMax(heights, stack, max, i);
+        }
+        stack.push(i);
+    }
+    while (!stack.isEmpty()) {
+        max = getMax(heights, stack, max, heights.length);
+    }
+    return max;
+}
+
+private int getMax(int[] heights, Stack<Integer> stack, int max, int i){
+    int area;
+    int popped =  stack.pop();
+    if(stack.isEmpty()){
+        area = heights[popped] * i;
+    }else{
+        area = heights[popped] * (i - 1 - stack.peek());
+    }
+    return Math.max(max, area);
+}
