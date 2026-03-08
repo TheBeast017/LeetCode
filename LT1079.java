37ee1b13121c7958498a52735159143d74f04dac
Kartikay Tewari <tewarinaman77@gmail.com>
2026-03-08 23:05:22 +0530

    Java File

---
+++ LT1079.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numTilePossibilities(String tiles) {
+        int[] count = new int[26];
+        for (char c : tiles.toCharArray()) count[c - 'A']++;
+        return dfs(count);
+    }
+    public int dfs(int[] count) {
+        int sum = 0;
+        for (int i = 0; i < 26; i++) {
+            if (count[i] == 0) continue;
+            sum++;
+            count[i]--;
+            sum += dfs(count);
+            count[i]++;
+        }
+        return sum;
+    }
+}
