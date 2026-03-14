5a79c7c6e63760e14d5d263b673885cb0737a57d
Kartikay Tewari <tewarinaman77@gmail.com>
2026-03-14 19:37:13 +0530

    Java File

---
+++ LT260.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xor = 0;
+        for (int n : nums) xor ^= n;
+        int diff = xor & -xor;
+        int a = 0, b = 0;
+        for(int n : nums){
+            if ((n & diff) == 0) a ^= n;
+            else b ^= n;
+        }
+        return new int[]{a, b};
+    }
+}
