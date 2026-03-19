fd06fbb7d20ff5b53e581baad1cb088ab1dce8cc
Kartikay Tewari <tewarinaman77@gmail.com>
2026-03-19 22:38:43 +0530

    Java file

---
+++ LT3074.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minimumBoxes(int[] apple, int[] capacity) {
+        int total = 0;
+        for (int a : apple) total += a;
+
+        Arrays.sort(capacity);
+        int used = 0, sum = 0;
+
+        for (int i = capacity.length - 1; i >= 0; i--) {
+            sum += capacity[i];
+            used++;
+            if (sum >= total) return used;
+        }
+
+        return used;
+    }
+}
