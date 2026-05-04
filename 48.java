ad5b7b6fc8268d29253b308a7fd4129a60ef6d42
Kartikay Tewari <tewarinaman77@gmail.com>
2026-05-04 23:26:31 +0530

    Java File

---
+++ 48.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        // first transpose the given matrix
+        int n = matrix.length;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if( i < j ){
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
+            }
+        }
+
+        // reverse the array
+
+        for(int i = 0; i < n; i++){
+            int start = 0, end = n-1;
+            while (start < end ){
+                int temp = matrix[i][start];
+                matrix[i][start++] = matrix[i][end];
+                matrix[i][end--] = temp;
+                
+            }
+        }
+    }
+}
