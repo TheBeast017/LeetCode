c753c365a59d25305020eba5724cfd58908ac1cb
Kartikay Tewari <tewarinaman77@gmail.com>
2026-07-07 22:25:04 +0530

    Time: 2 ms (29.75%) | Memory: 42.6 MB (67.71%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -1,15 +1,15 @@
 class Solution {
     public long sumAndMultiply(int n) {
-        long ans = 0, nN = 0, org = n;
-        boolean noZero = true;
+        long ans = 0, nN = 0;//, org = n;
+        // boolean noZero = true;
         while (n != 0) {
             if (n % 10 != 0){
                 ans += n % 10;
                 nN = (nN * 10) + (n % 10);
-            }else noZero = false;
+            } //else noZero = false;
             n /= 10;
         }
-        if (noZero) return org * ans;
+        // if (noZero) return org * ans;
         return Integer.parseInt(new StringBuilder(String.valueOf(nN)).reverse().toString()) * ans;
 
     }
